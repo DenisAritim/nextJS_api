948ff11a3897f3979d69a04c9456c576fa252dc4
DenisParise <dd.parise@gmail.com>
2024-08-09 15:54:59 +0200

    localization: fixed localization error in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function About() {
     return (
         <div>
-            <div className="bg-red-500">Ciao frocio</div>
+            <div className="bg-red-500">Herro Wordo</div>
             <Link href={"/"}>Home</Link>
         </div>
     );
